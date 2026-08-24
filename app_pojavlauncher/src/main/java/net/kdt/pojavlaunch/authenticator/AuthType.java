5bd50964d99618992be40548a80eeb4b75b7e070
skynecos <skynecos@gmail.com>
2026-08-24 10:35:05 +0300

    Remove legacy cleartext skin endpoint

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/authenticator/AuthType.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/authenticator/AuthType.java
@@ -13,14 +13,14 @@ public enum AuthType {
             MicrosoftBackgroundLogin.CREATOR,
             R.drawable.ic_auth_ms,
             null,
-            "https://mineskin.eu/skin/%s" // Switched from mc-heads.net cause blocked in Russia
+            "https://mineskin.eu/skin/%s"
     ),
     @SerializedName("elyby")
     ELY_BY(
             ElyByBackgroundLogin.CREATOR,
             R.drawable.ic_auth_elyby,
             "ely.by",
-            "http://skinsystem.ely.by/skins/%s.png"
+            null // Do not fetch launcher avatars over the legacy cleartext skin endpoint.
     ),
     @SerializedName("local")
     LOCAL(null, 0, null, null);
