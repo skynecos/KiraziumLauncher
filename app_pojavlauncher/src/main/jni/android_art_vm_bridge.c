ad7bf8a8ed10f32014a8d018235c50e648f13bf6
skynecos <skynecos@gmail.com>
2026-09-09 06:49:35 +0300

    fix(android): format ART JavaVM pointer portably

--- app_pojavlauncher/src/main/jni/android_art_vm_bridge.c
+++ app_pojavlauncher/src/main/jni/android_art_vm_bridge.c
@@ -1,6 +1,5 @@
 #include <jni.h>
 #include <android/log.h>
-#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -12,8 +11,8 @@ static void publish_android_art_vm(JavaVM *vm) {
     if (vm == NULL) return;
     g_android_art_vm = vm;
 
-    char value[2 + sizeof(uintptr_t) * 2 + 1];
-    snprintf(value, sizeof(value), "0x%" PRIxPTR, (uintptr_t) vm);
+    char value[32];
+    snprintf(value, sizeof(value), "%p", (void *) vm);
     setenv("KIRAZIUM_ANDROID_ART_VM", value, 1);
     __android_log_print(ANDROID_LOG_INFO, TAG,
                         "Published Android ART JavaVM for DreamDisplays MediaCodec: %s", value);
