c5aebbddf81d0c292ff889b5a1c08a4d41acc74a
skynecos <skynecos@gmail.com>
2026-09-10 02:54:13 +0300

    fix: install embedded DreamDisplays build at launcher startup

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/PojavApplication.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/PojavApplication.java
@@ -15,6 +15,7 @@ import android.util.Log;
 import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
 
+import net.kdt.pojavlaunch.instances.KiraziumBootstrap;
 import net.kdt.pojavlaunch.lifecycle.ContextExecutor;
 import net.kdt.pojavlaunch.prefs.LauncherPreferences;
 import net.kdt.pojavlaunch.tasks.AsyncAssetManager;
@@ -25,7 +26,6 @@ import net.kdt.pojavlaunch.utils.LocaleUtils;
 
 import java.io.File;
 import java.io.PrintStream;
-
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
@@ -38,40 +38,48 @@ import git.artdeell.mojo.BuildConfig;
 import git.artdeell.mojo.R;
 
 public class PojavApplication extends Application {
-	public static final String CRASH_REPORT_TAG = "PojavCrashReport";
-	public static final ExecutorService sExecutorService = new ThreadPoolExecutor(4, 4, 500, TimeUnit.MILLISECONDS,  new LinkedBlockingQueue<>());
+    public static final String CRASH_REPORT_TAG = "PojavCrashReport";
+    public static final ExecutorService sExecutorService = new ThreadPoolExecutor(4, 4, 500,
+            TimeUnit.MILLISECONDS, new LinkedBlockingQueue<>());
 
-	private void installFatalErrorHandler() {
-		Thread.setDefaultUncaughtExceptionHandler((thread, th) -> {
-			boolean storagePermAllowed = (Build.VERSION.SDK_INT < 23 || Build.VERSION.SDK_INT >= 29 ||
-					ActivityCompat.checkSelfPermission(PojavApplication.this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) && Tools.checkStorageRoot(PojavApplication.this);
-			File crashFile = new File(storagePermAllowed ? Tools.DIR_GAME_HOME : Tools.DIR_DATA, "latestcrash.txt");
-			try {
-				FileUtils.ensureParentDirectory(crashFile);
-				PrintStream crashStream = new PrintStream(crashFile);
-				crashStream.append(getString(R.string.app_short_name)).append(" crash report\n");
-				SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss z", Locale.US);
-				crashStream.append(" - Time: ").append(sdf.format(new Date())).append("\n");
-				crashStream.append(" - Device: ").append(Build.PRODUCT).append(" ").append(Build.MODEL).append("\n");
-				crashStream.append(" - Android version: ").append(Build.VERSION.RELEASE).append("\n");
-				crashStream.append(" - Launcher version: " + BuildConfig.VERSION_NAME + "\n");
-				crashStream.append(" - Build type: " + BuildConfig.BUILD_TYPE + "\n");
-				crashStream.append(" - Crash stack trace:\n");
-				crashStream.append(Log.getStackTraceString(th));
-				crashStream.close();
-			} catch (Throwable throwable) {
-				Log.e(CRASH_REPORT_TAG, " - Exception attempt saving crash stack trace:", throwable);
-				Log.e(CRASH_REPORT_TAG, " - The crash stack trace was:", th);
-			}
+    private void installFatalErrorHandler() {
+        Thread.setDefaultUncaughtExceptionHandler((thread, th) -> {
+            boolean storagePermAllowed = (Build.VERSION.SDK_INT < 23 ||
+                    Build.VERSION.SDK_INT >= 29 || ActivityCompat.checkSelfPermission(
+                    PojavApplication.this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE) ==
+                    PackageManager.PERMISSION_GRANTED) && Tools.checkStorageRoot(PojavApplication.this);
+            File crashFile = new File(storagePermAllowed ? Tools.DIR_GAME_HOME : Tools.DIR_DATA,
+                    "latestcrash.txt");
+            try {
+                FileUtils.ensureParentDirectory(crashFile);
+                PrintStream crashStream = new PrintStream(crashFile);
+                crashStream.append(getString(R.string.app_short_name)).append(" crash report\n");
+                SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss z", Locale.US);
+                crashStream.append(" - Time: ").append(sdf.format(new Date())).append("\n");
+                crashStream.append(" - Device: ").append(Build.PRODUCT).append(" ")
+                        .append(Build.MODEL).append("\n");
+                crashStream.append(" - Android version: ").append(Build.VERSION.RELEASE)
+                        .append("\n");
+                crashStream.append(" - Launcher version: " + BuildConfig.VERSION_NAME + "\n");
+                crashStream.append(" - Build type: " + BuildConfig.BUILD_TYPE + "\n");
+                crashStream.append(" - Crash stack trace:\n");
+                crashStream.append(Log.getStackTraceString(th));
+                crashStream.close();
+            } catch (Throwable throwable) {
+                Log.e(CRASH_REPORT_TAG, " - Exception attempt saving crash stack trace:", throwable);
+                Log.e(CRASH_REPORT_TAG, " - The crash stack trace was:", th);
+            }
 
-			FatalErrorActivity.showError(PojavApplication.this, crashFile.getAbsolutePath(), storagePermAllowed, th);
-			Tools.fullyExit();
-		});
-	}
+            FatalErrorActivity.showError(PojavApplication.this, crashFile.getAbsolutePath(),
+                    storagePermAllowed, th);
+            Tools.fullyExit();
+        });
+    }
 
     private void registerKiraziumUpdaterLifecycle() {
         registerActivityLifecycleCallbacks(new ActivityLifecycleCallbacks() {
-            @Override public void onActivityCreated(@NonNull Activity activity, Bundle savedInstanceState) {}
+            @Override public void onActivityCreated(@NonNull Activity activity,
+                                                     Bundle savedInstanceState) {}
             @Override public void onActivityStarted(@NonNull Activity activity) {}
 
             @Override
@@ -83,52 +91,52 @@ public class PojavApplication extends Application {
 
             @Override public void onActivityPaused(@NonNull Activity activity) {}
             @Override public void onActivityStopped(@NonNull Activity activity) {}
-            @Override public void onActivitySaveInstanceState(@NonNull Activity activity, @NonNull Bundle outState) {}
+            @Override public void onActivitySaveInstanceState(@NonNull Activity activity,
+                                                               @NonNull Bundle outState) {}
             @Override public void onActivityDestroyed(@NonNull Activity activity) {}
         });
     }
 
-	@Override
-	public void onCreate() {
-		ContextExecutor.setApplication(this);
-		// Disable fatal errors on gplay. This is necessary so that google can collect crash report data and send it to me
-		// (where i can find the cause and fix it)
+    @Override
+    public void onCreate() {
+        ContextExecutor.setApplication(this);
+        // Disable fatal errors on gplay so Google can collect the crash report data.
         //noinspection ConstantValue
-        if(!BuildConfig.BUILD_TYPE.equals("gplay")) installFatalErrorHandler();
-		
-		try {
-			super.onCreate();
+        if (!BuildConfig.BUILD_TYPE.equals("gplay")) installFatalErrorHandler();
+
+        try {
+            super.onCreate();
             registerKiraziumUpdaterLifecycle();
-			if(Tools.checkStorageRoot(this)){
-				LauncherPreferences.loadPreferences(this);
-			} else {
-				Tools.initEarlyConstants(this);
-			}
-			Tools.DEVICE_ARCHITECTURE = Architecture.getDeviceArchitecture();
-			//Force x86 lib directory for Asus x86 based zenfones
-			if(Architecture.isx86Device() && Architecture.is32BitsDevice()){
-				String originalJNIDirectory = getApplicationInfo().nativeLibraryDir;
-				getApplicationInfo().nativeLibraryDir = originalJNIDirectory.substring(0,
-												originalJNIDirectory.lastIndexOf("/"))
-												.concat("/x86");
-			}
+            if (Tools.checkStorageRoot(this)) {
+                LauncherPreferences.loadPreferences(this);
+                KiraziumBootstrap.ensureEmbeddedCinemaMod(this);
+            } else {
+                Tools.initEarlyConstants(this);
+            }
+            Tools.DEVICE_ARCHITECTURE = Architecture.getDeviceArchitecture();
+            // Force x86 lib directory for Asus x86 based zenfones.
+            if (Architecture.isx86Device() && Architecture.is32BitsDevice()) {
+                String originalJNIDirectory = getApplicationInfo().nativeLibraryDir;
+                getApplicationInfo().nativeLibraryDir = originalJNIDirectory.substring(0,
+                        originalJNIDirectory.lastIndexOf("/")).concat("/x86");
+            }
             MoJsonDownloader.prepareSubstitutionMap(getAssets());
-			AsyncAssetManager.unpackRuntime(getAssets());
-		} catch (Throwable throwable) {
-			Intent ferrorIntent = new Intent(this, FatalErrorActivity.class);
-			ferrorIntent.putExtra("throwable", throwable);
-			ferrorIntent.setFlags(FLAG_ACTIVITY_NEW_TASK);
-			startActivity(ferrorIntent);
-		}
-	}
+            AsyncAssetManager.unpackRuntime(getAssets());
+        } catch (Throwable throwable) {
+            Intent ferrorIntent = new Intent(this, FatalErrorActivity.class);
+            ferrorIntent.putExtra("throwable", throwable);
+            ferrorIntent.setFlags(FLAG_ACTIVITY_NEW_TASK);
+            startActivity(ferrorIntent);
+        }
+    }
 
-	@Override
-	public void onTerminate() {
-		super.onTerminate();
-		ContextExecutor.clearApplication();
-	}
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+        ContextExecutor.clearApplication();
+    }
 
-	@Override
+    @Override
     protected void attachBaseContext(Context base) {
         super.attachBaseContext(LocaleUtils.setLocale(base));
     }
